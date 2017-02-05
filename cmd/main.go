25b64d1f80aeb7e3b32018f05a6cfa5a2b5f7dc4
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-02-05 20:24:34 +0000

    Added answers to quiz 3.

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,15 @@ var quiz = flag.Int("quiz", 0, "The quiz to return the answers for.")
 func main() {
 	flag.Parse()
 
-	fmt.Printf("Printing answers for quiz %d.\n", quiz)
+	fmt.Printf("Printing answers for quiz %d.\n", *quiz)
 
 	switch *quiz {
 	case 1:
 		quiz1()
 	case 2:
 		quiz2()
+	case 3:
+		quiz3()
 	default:
 		fmt.Println("Quiz not found.")
 	}
@@ -47,4 +49,31 @@ func quiz2() {
 	fmt.Println(q3)
 	q4 := linear.NewVector(1.996, 3.108, -4.554).Normalize()
 	fmt.Println(q4)
+}
+
+func quiz3() {
+	q1, err := linear.NewVector(7.887, 4.138).DotProduct(linear.NewVector(-8.802, 6.776))
+	if err != nil {
+		fmt.Println("Failed to answer question 1")
+		return
+	}
+	fmt.Println(q1)
+	q2, err := linear.NewVector(-5.955, -4.904, -1.874).DotProduct(linear.NewVector(-4.496, -8.755, 7.103))
+	if err != nil {
+		fmt.Println("Failed to answer question 2")
+		return
+	}
+	fmt.Println(q2)
+	q3, err := linear.NewVector(3.183, -7.627).AngleBetween(linear.NewVector(-2.668, 5.319))
+	if err != nil {
+		fmt.Println("Failed to answer question 3")
+		return
+	}
+	fmt.Println(q3)
+	q4, err := linear.NewVector(7.35, 0.221, 5.188).AngleBetween(linear.NewVector(2.751, 8.259, 3.985))
+	if err != nil {
+		fmt.Println("Failed to answer question 4")
+		return
+	}
+	fmt.Println(q4.Degrees())
 }
