9bcfad781dfafa4a3f673e39d966bbe4320c67a9
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-02-11 20:51:59 +0000

    Added big.Float support for tolerances.

--- tolerance/within.go
+++ tolerance/within.go
@@ -1,5 +1,7 @@
 package tolerance
 
+import "math/big"
+
 const (
 	// OneDecimalPlace represents a distance of 1dp.
 	OneDecimalPlace = float64(0.15)
@@ -8,6 +10,11 @@ const (
 	// ThreeDecimalPlaces represents a distance of 3dp.
 	ThreeDecimalPlaces = float64(0.0015)
 )
+
+// DecimalPlaces is the number of decimal places to use.
+func DecimalPlaces(number int) float64 {
+	return 1.5 / float64(number*10)
+}
 
 // IsWithin returns true when the parameters are within the given tolerance from each other.
 func IsWithin(a float64, b float64, tolerance float64) bool {
@@ -19,4 +26,18 @@ func distance(a float64, b float64) float64 {
 		return a - b
 	}
 	return b - a
+}
+
+// IsWithinBig returns true when the parameters are within the given tolerance from each other.
+func IsWithinBig(a *big.Float, b *big.Float, tolerance float64) bool {
+	return distanceBig(a, b).Cmp(big.NewFloat(tolerance)) <= 0
+}
+
+func distanceBig(a *big.Float, b *big.Float) *big.Float {
+	r := &big.Float{}
+
+	if a.Cmp(b) > 0 {
+		return r.Sub(a, b)
+	}
+	return r.Sub(b, a)
 }
--- tolerance/within_test.go
+++ tolerance/within_test.go
@@ -1,6 +1,9 @@
 package tolerance
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func TestIsWithin(t *testing.T) {
 	tests := []struct {
@@ -103,6 +106,120 @@ func TestIsWithin(t *testing.T) {
 
 	for _, test := range tests {
 		actual := IsWithin(test.a, test.b, test.tolerance)
+
+		if actual != test.expected {
+			t.Errorf("For ||%f-%f|| within %f, expected %v, but got %v", test.a, test.b, test.tolerance, test.expected, actual)
+		}
+	}
+}
+
+func TestIsWithinBig(t *testing.T) {
+	tests := []struct {
+		a         *big.Float
+		b         *big.Float
+		tolerance float64
+		expected  bool
+	}{
+		{
+			a:         big.NewFloat(1.0),
+			b:         big.NewFloat(1.1),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(1),
+			b:         big.NewFloat(1.2),
+			tolerance: OneDecimalPlace,
+			expected:  false,
+		},
+		{
+			a:         big.NewFloat(1),
+			b:         big.NewFloat(1.11),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(1),
+			b:         big.NewFloat(1.16),
+			tolerance: OneDecimalPlace,
+			expected:  false,
+		},
+		{
+			a:         big.NewFloat(-1),
+			b:         big.NewFloat(-1.1),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(-1),
+			b:         big.NewFloat(-1.2),
+			tolerance: OneDecimalPlace,
+			expected:  false,
+		},
+		{
+			a:         big.NewFloat(-1),
+			b:         big.NewFloat(-1.05),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(-1.5),
+			b:         big.NewFloat(1.5),
+			tolerance: float64(3),
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(-1.55),
+			b:         big.NewFloat(-1.5),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(-1.5),
+			b:         big.NewFloat(-1.55),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.1),
+			tolerance: OneDecimalPlace,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.1),
+			tolerance: TwoDecimalPlaces,
+			expected:  false,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.14),
+			tolerance: TwoDecimalPlaces,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.14),
+			tolerance: ThreeDecimalPlaces,
+			expected:  false,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.141),
+			tolerance: ThreeDecimalPlaces,
+			expected:  true,
+		},
+		{
+			a:         big.NewFloat(3.14159265359),
+			b:         big.NewFloat(3.1415926536),
+			tolerance: DecimalPlaces(10),
+			expected:  true,
+		},
+	}
+
+	for _, test := range tests {
+		actual := IsWithinBig(test.a, test.b, test.tolerance)
 
 		if actual != test.expected {
 			t.Errorf("For ||%f-%f|| within %f, expected %v, but got %v", test.a, test.b, test.tolerance, test.expected, actual)
